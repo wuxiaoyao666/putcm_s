f972dee4318c0cb39fcc68e52015ca92ee10e52c
MaoShanShan <Maolizhi1210@163.com>
2025-11-20 16:53:08 +0800

    化学成分

--- app/models/huaxue.py
+++ app/models/huaxue.py
@@ -103,6 +103,106 @@ class Huaxue(Base):
         items = items_res.mappings().all()
         return {"total": total, "items": items}
 
+    @staticmethod
+    async def get(db: AsyncSession, subId: int):
+        query = (
+            select(Huaxue.t1, Huaxue.t2, Huaxue.t3, Huaxue.t4, Huaxue.t5, Huaxue.t6, Huaxue.t7, Huaxue.t8, Huaxue.t9, Huaxue.t10, Huaxue.t11,
+                   Huaxue.t12, Huaxue.t13, Huaxue.t14, Huaxue.t15, Huaxue.t16, Huaxue.t17, Huaxue.t18, Huaxue.t19, Huaxue.t20, Huaxue.t21,
+                   func.JSON_LENGTH(Huaxue.i1).label("si1"), Huaxue.t22, Huaxue.t23,
+                   User.userName.label("updateUser"), Huaxue.updateTime)
+            .select_from(Huaxue)
+            .outerjoin(User, Huaxue.userId == User.userId)
+            .where(Huaxue.subId == subId))
+        res = await db.execute(query)
+        item = res.mappings().one_or_none()
+        return item
+
+    @staticmethod
+    async def get_i1(db: AsyncSession, subId: int):
+        query = select(Huaxue.i1).where(Huaxue.subId == subId)
+        data = await db.scalar(query)
+        return data
+
+    @staticmethod
+    async def edit(db: AsyncSession, subId: int, item: Huaxue, userId: int):
+        # 检查图片是否存在
+        if item.i1 and len(item.i1) > 0:
+            for sub in item.i1:
+                localPath = sub.img[len("/api/"):]
+                if not await os.path.isfile(localPath):
+                    raise FileNotFoundError(f"{sub.title} 图片不存在")
+        # 读旧的i1
+        old_i1_query = select(Huaxue.i1).where(Huaxue.subId == subId)
+        old_i1: Optional[List[ImageTitle]] = await db.scalar(old_i1_query)
+        # 新旧比较并做处理，如果不行，则类型出了问题
+        await ImageTitle.edit_compare(db, item.i1, old_i1)
+        # 转换为有效dict,不能过滤掉空值，因为空值有意义
+        values = item.model_dump()
+        values['userId'] = userId
+        now = int(datetime.now().timestamp())
+        values["updateTime"] = now
+        query = update(Huaxue).where(Huaxue.subId == subId).values(values)
+        await db.execute(query)
+        await db.commit()
+
+    @staticmethod
+    async def delete(db: AsyncSession, subId: int):
+        old_i1_query = select(Huaxue.i1).where(Huaxue.subId == subId)
+        old_i1: Optional[List] = await db.scalar(old_i1_query)
+        if old_i1 and len(old_i1) > 0:
+            for old_sub in old_i1:
+                localPath = old_sub.get("img")[len("/api/"):]
+                if Path(localPath).exists():
+                    await async_os.remove(localPath)
+        query = delete(Huaxue).where(Huaxue.subId == subId)
+        await db.execute(query)
+        await db.commit()
+
+    @staticmethod
+    async def export(db: AsyncSession, tcmId: int) -> Tuple[str, str, io.BytesIO]:
+        tcmName = await Tcm.get_tcmName(db, tcmId)
+        subName = "4_化学成分"
+        query = select(Huaxue).where(Huaxue.tcmId == tcmId)
+        res = await db.execute(query)
+        records = res.scalars().all()
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            excel_data = []
+            index = 1
+            for record in records:
+                excel_data.append({
+                    "序号": index,
+                    "药材来源": ImageTitle.parse_html(record.t1),
+                    "来源部位": ImageTitle.parse_html(record.t2),
+                    "药材编号": ImageTitle.parse_html(record.t3),
+                    "化合物名称": ImageTitle.parse_html(record.t4),
+                    "化合物描述": ImageTitle.parse_html(record.t5),
+                    "英文名称": ImageTitle.parse_html(record.t6),
+                    "分子式": ImageTitle.parse_html(record.t7),
+                    "分子量(g/mol)": ImageTitle.parse_html(record.t8),
+                    "SMILES结构式": ImageTitle.parse_html(record.t9),
+                    "理化性质": ImageTitle.parse_html(record.t10),
+                    "化学结构分类": ImageTitle.parse_html(record.t11),
+                    "紫外(UV)光谱": ImageTitle.parse_html(record.t12),
+                    "红外(IR)光谱": ImageTitle.parse_html(record.t13),
+                    "圆二色(CD)光谱": ImageTitle.parse_html(record.t14),
+                    "核磁共振氢谱（1H NMR）": ImageTitle.parse_html(record.t15),
+                    "核磁共振碳谱（13C NMR）": ImageTitle.parse_html(record.t16),
+                    "提取方式": ImageTitle.parse_html(record.t17),
+                    "含量范围（%或mg/g）": ImageTitle.parse_html(record.t18),
+                    "含量变化因素": ImageTitle.parse_html(record.t19),
+                    "药理活性描述": ImageTitle.parse_html(record.t20),
+                    "作用靶点/通路": ImageTitle.parse_html(record.t21),
+                    "分子结构图": f"{len(record.i1) if record.i1 else 0}张",
+                    "文献/数据库来源": ImageTitle.parse_html(record.t22),
+                    "备注说明": ImageTitle.parse_html(record.t23),
+                })
+                # 处理图片，批量复制多个文件到临时目录，并重新命名
+                await ImageTitle.tcm_copy_file(record.i1, temp_dir, tcmName, subName, index)
+                index += 1
+            zip_buffer = await ImageTitle.export_zip(temp_dir, tcmName, subName, excel_data)
+            return tcmName, subName, zip_buffer
+
 
 class HuaxueReq(BaseModel):
     t1: Optional[str] = None
---
+++ app/routers/huaxue.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, UploadFile, File, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import get_db
+from app.models.tmpfile import TmpFile
+from app.models.huaxue import Huaxue, HuaxueReq, HuaxueQueryParams
+from app.models.user import User, get_curr_user
+from app.utils.imgutil import ImageTitle
+
+router = APIRouter(prefix="/api/huaxue", tags=["huaxue"])
+
+
+@router.post("/img")
+async def up_img(tcmId: int, file: UploadFile = File(...), db: AsyncSession = Depends(get_db)):
+    img = await TmpFile.add(db, file, f"/{tcmId}", "上传原植物图片")
+    return {"status": 0, "data": {"value": img}}
+
+
+@router.post("")
+async def add(tcmId: int, item: HuaxueReq, curr_user: User = Depends(get_curr_user), db: AsyncSession = Depends(get_db)):
+    await Huaxue.add(db, tcmId, item, curr_user.userId)
+    return {"status": 0}
+
+
+@router.get("/list")
+async def get_list(tcmId: int, page: int = Query(1), perPage: int = Query(10), params: HuaxueQueryParams = Depends(),
+                   db: AsyncSession = Depends(get_db)):
+    data = await Huaxue.get_list(db, tcmId, page, perPage, params)
+    return {"status": 0, "data": data}
+
+
+@router.get("")
+async def get(subId: int, db: AsyncSession = Depends(get_db)):
+    data = await Huaxue.get(db, subId)
+    return {"status": 0, "data": data}
+
+
+@router.get("/i1")
+async def get(subId: int, db: AsyncSession = Depends(get_db)):
+    data = await Huaxue.get_i1(db, subId)
+    return {"status": 0, "data": data}
+
+
+@router.put("")
+async def edit(subId: int, item: HuaxueReq, curr_user: User = Depends(get_curr_user),
+               db: AsyncSession = Depends(get_db)):
+    await Huaxue.edit(db, subId, item, curr_user.userId)
+    return {"status": 0}
+
+
+@router.delete("")
+async def delete(subId: int, db: AsyncSession = Depends(get_db)):
+    await Huaxue.delete(db, subId)
+    return {"status": 0}
+
+
+@router.get("/export")
+async def export(tcmId: int, db: AsyncSession = Depends(get_db)):
+    tcmName, subName, zip_file = await Huaxue.export(db, tcmId)
+    return ImageTitle.resp_zip(tcmName, subName, zip_file)
